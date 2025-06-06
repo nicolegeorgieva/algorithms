a50294f8fe46db0ac2b845769d869785e43b5710
Nicole <nicole.georgievva@gmail.com>
2025-06-06 18:14:09 +0300

    Solve merge

--- src/main/kotlin/mergeSortedArray.kt
+++ src/main/kotlin/mergeSortedArray.kt
@@ -1,9 +1,10 @@
 fun main() {
-  val arr1 = intArrayOf(0, 0, 0)
+  // 1, 2, 2, 3, 5, 6
+  val arr1 = intArrayOf(1, 2, 3, 0, 0, 0)
   val arr2 = intArrayOf(2, 5, 6)
   merge(
     nums1 = arr1,
-    m = 0,
+    m = 3,
     nums2 = arr2,
     n = arr2.size,
   )
@@ -16,18 +17,24 @@ fun merge(
   nums2: IntArray,
   n: Int,
 ) {
-  if (n == 0) return
-  if (m == 0) {
-    for (index in nums1.indices) {
-      nums1[index] = nums2[index]
+  var p1 = m - 1
+  var p2 = n - 1
+  var pMerged = m + n - 1
+
+  while (p1 >= 0 && p2 >= 0) {
+    if (nums1[p1] > nums2[p2]) {
+      nums1[pMerged] = nums1[p1]
+      p1--
+    } else {
+      nums1[pMerged] = nums2[p2]
+      p2--
     }
-    return
+    pMerged--
   }
-  if (m >= n) {
-    for (firstArrIndex in nums1.indices) {
 
-    }
-  } else {
-
+  while (p2 >= 0) {
+    nums1[pMerged] = nums2[p2]
+    p2--
+    pMerged--
   }
 }
