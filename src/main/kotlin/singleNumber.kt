5dc1d16decab7678b4e1510b367663104b1f0c6c
Nicole <nicole.georgievva@gmail.com>
2025-06-17 23:07:33 +0300

    Optimize singleNumber()

--- src/main/kotlin/singleNumber.kt
+++ src/main/kotlin/singleNumber.kt
@@ -1,22 +1,20 @@
 fun main() {
-  val nums = intArrayOf(2, 2, 1)
+  val nums = intArrayOf(4, 4, 1, 2, 2)
   // 1
   println(singleNumber(nums))
 }
 
 // [4,1,2,1,2] -> 4
 private fun singleNumber(nums: IntArray): Int {
-  val numbersMap = mutableMapOf(
-    nums[0] to 1
-  )
+  val numbersMap = mutableMapOf<Int, Int>()
 
-  for (i in 1..nums.size - 1) {
-    if (numbersMap[nums[i]] != null) {
-      numbersMap[nums[i]] = numbersMap[nums[i]]?.plus(1)!!
-    } else {
-      numbersMap.set(key = nums[i], value = 1)
-    }
+  for (n in nums) {
+    numbersMap[n] = (numbersMap[n] ?: 0) + 1
   }
 
-  return numbersMap.entries.find { it.value == 1 }?.key ?: 0
+  for (entry in numbersMap) {
+    if (entry.value == 1) return entry.key
+  }
+
+  return 0
 }
