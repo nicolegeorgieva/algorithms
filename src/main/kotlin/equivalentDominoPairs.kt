5de42ad70051d678343dd977fc922bd38f249087
Nicole <nicole.georgievva@gmail.com>
2025-10-03 14:50:48 +0300

    Solve equivalentDominoPairs()

--- src/main/kotlin/equivalentDominoPairs.kt
+++ src/main/kotlin/equivalentDominoPairs.kt
@@ -1,5 +1,11 @@
 fun main() {
+  val dominoes = arrayOf(intArrayOf(1, 2), intArrayOf(2, 1), intArrayOf(3, 4), intArrayOf(5, 6))
+  val dominoes2 = arrayOf(intArrayOf(1, 2), intArrayOf(1, 2), intArrayOf(1, 1), intArrayOf(1, 2), intArrayOf(2, 2))
 
+  // 1
+  println(numEquivDominoPairs(dominoes))
+  // 3
+  println(numEquivDominoPairs(dominoes2))
 }
 
 /*
@@ -8,24 +14,41 @@ e.g. Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
 => Output: 1
  */
 private fun numEquivDominoPairs(dominoes: Array<IntArray>): Int {
-  val uniquePairs = mutableMapOf<IntArray, Int>()
+  val equivPairs = mutableMapOf<IntArray, Int>()
   var index = 0
 
   while (index < dominoes.size) {
-
-    for (i in 0 until dominoes.size) {
-      if (i == index) continue
-      if (isEqual(dominoes[index],dominoes[i])) {
-        uniquePairs[dominoes[index]] = uniquePairs[dominoes[index]]?.plus(1) ?: 2
+    if (index == dominoes.size - 1) {
+      break
+    }
+    val currentPair = sortPair(dominoes[index])
+    if (equivPairs[currentPair] != null) {
+      index++
+      continue
+    }
+    for (i in index + 1 until dominoes.size) {
+      if (isEqual(currentPair, dominoes[i])) {
+        equivPairs[currentPair] = equivPairs[currentPair]?.plus(1) ?: 1
       }
     }
     index++
   }
 
-  TODO()
+  return equivPairs.values.sum()
 }
 
 private fun isEqual(firstPair: IntArray, secondPair: IntArray): Boolean {
-  return (firstPair[0] == secondPair[0] && firstPair[1] == secondPair[1]) ||
-      (firstPair[0] == secondPair[1] && firstPair[1] == secondPair[0])
+  return firstPair[0] == secondPair[0] && firstPair[1] == secondPair[1] ||
+      firstPair[0] == secondPair[1] && firstPair[1] == secondPair[0]
+}
+
+private fun sortPair(pair: IntArray): IntArray {
+  if (pair.size > 2) return pair
+  if (pair[0] == pair[1]) return pair
+
+  return if (pair[0] < pair[1]) {
+    pair
+  } else {
+    intArrayOf(pair[1], pair[0])
+  }
 }
