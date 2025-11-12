52b648b1fa03ec32a59f8972af2cbb688f58c466
Nicole <nicole.georgievva@gmail.com>
2025-11-12 16:25:31 +0200

    Optimize isPalindrome()

--- src/main/kotlin/validPalindrome.kt
+++ src/main/kotlin/validPalindrome.kt
@@ -4,9 +4,29 @@ fun main() {
 }
 
 private fun isPalindrome(s: String): Boolean {
-  val word = s.lowercase().filter {
-    it in 'a'..'z' || it in '0'..'9'
+  var left = 0
+  var right = s.lastIndex
+
+  while (left < right) {
+    while (left in s.indices && !s[left].isAllowed()) {
+      left++
+    }
+    while (right in s.indices && !s[right].isAllowed()) {
+      right--
+    }
+    if (left == right || left !in s.indices || right !in s.indices) return true
+    if (s[left].lowercase() == s[right].lowercase()) {
+      left++
+      right--
+      continue
+    } else {
+      return false
+    }
   }
 
-  return word == word.reversed()
+  return true
+}
+
+private fun Char.isAllowed(): Boolean {
+  return this in 'A'..'Z' || this in 'a'..'z' || this in '0'..'9'
 }
