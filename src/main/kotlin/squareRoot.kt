924cfd20b5ada4dc61b563bdfeec6bf26e8c430d
Nicole <nicole.georgievva@gmail.com>
2026-01-06 16:48:12 +0200

    Solve mySqrt()

---
+++ src/main/kotlin/squareRoot.kt
@@ -0,0 +1,28 @@
+fun main() {
+  // 2
+  println(mySqrt(2147483647))
+  // 2
+  println(mySqrt(4))
+  // 2
+  println(mySqrt(8))
+  // 3
+  println(mySqrt(10))
+  // 10
+  println(mySqrt(100))
+  // 90
+  println(mySqrt(8192))
+  // 46339
+  println(mySqrt(2147395599))
+}
+
+// Returns the square root of x rounded down to the nearest integer
+// Res should not be negative
+private fun mySqrt(x: Int): Int {
+  val t = x.toLong()
+  var num = x.toLong()
+  while (num * num > t) {
+    num = (num + (t / num)) / 2
+  }
+
+  return num.toInt()
+}
