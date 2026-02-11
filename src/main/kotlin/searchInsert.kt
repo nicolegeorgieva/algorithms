463fc3d2e0bf0bcbde55901ef5271c56044d355e
Nicole <nicole.georgievva@gmail.com>
2026-02-11 14:23:10 +0200

    Optimize searchInsert algorithm

--- src/main/kotlin/searchInsert.kt
+++ src/main/kotlin/searchInsert.kt
@@ -14,12 +14,19 @@ Given a sorted array of distinct integers and a target value, return the index i
 If not, return the index where it would be if it were inserted in order.
  */
 private fun searchInsert(nums: IntArray, target: Int): Int {
-  for (i in nums.indices) {
-    if (i == 0 && nums[i] > target) return 0
-    if (nums[i] == target) return i
-    if (i == nums.lastIndex) return i + 1
-    if (nums[i + 1] > target) return i + 1
+  var l = 0
+  var r = nums.size - 1
+
+  while (l <= r) {
+    val mid = (l + r) / 2
+    if (nums[mid] == target) {
+      return mid
+    } else if (nums[mid] > target) {
+      r = mid - 1
+    } else {
+      l = mid + 1
+    }
   }
 
-  return -1
+  return l
 }
