260cca6196d92d37f13c14bda23ed6b8be88b25c
Nicole <nicole.georgievva@gmail.com>
2026-02-18 15:46:19 +0200

    Add another example for using .myApply()

--- src/main/kotlin/extensionfunctions/myApply.kt
+++ src/main/kotlin/extensionfunctions/myApply.kt
@@ -7,12 +7,16 @@ fun main() {
   val example2 = "Good afternoon!".myApply {
     println("The greeting is ${this.uppercase()}")
   }
-  // The greeting is Hello!
-  // The greeting is Good afternoon!
-  // Hello!
   println(example1)
-  // Good afternoon!
   println(example2)
+
+  val list = mutableListOf<Int>().myApply {
+    add(1)
+    add(2)
+    remove(1)
+    addAll(listOf(4, 5))
+  }
+  println(list)
 }
 
 private fun <T> T.myApply(block: T.() -> Unit): T {
