e9293ebb27990db5214935d2819f98ef9c18245c
Nicole <nicole.georgievva@gmail.com>
2026-02-27 14:20:04 +0200

    Optimize largestEven()

--- src/main/kotlin/largestEven.kt
+++ src/main/kotlin/largestEven.kt
@@ -5,11 +5,17 @@ fun main() {
   println(largestEven("221"))
   // ""
   println(largestEven("1"))
+  // 1112
+  println(largestEven2("1112"))
+  // 22
+  println(largestEven2("221"))
+  // ""
+  println(largestEven2("1"))
 }
 
 private fun largestEven(s: String): String {
   var newString = s
-  for (i in s.length - 1 downTo 0) {
+  for (i in newString.length - 1 downTo 0) {
     if (newString[i].digitToInt() != 2) {
       newString = newString.dropLast(1)
     } else {
@@ -19,3 +25,5 @@ private fun largestEven(s: String): String {
 
   return newString
 }
+
+private fun largestEven2(s: String): String = s.dropLastWhile { it.digitToInt() != 2 }
